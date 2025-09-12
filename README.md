a91746400cfb80069a6a75c43941303b41ff4b47
Daniela Plata <dplatatrabajos@gmail.com>
2025-09-11 21:24:49 -0500

    cosas

--- README.md
+++ README.md
@@ -17,5 +17,22 @@ Este trabajo fue desarrollado como parte del curso Analítica de Negocios en la 
 # CONTENIDO
 
 ## Lenguaje
+El proyecto está desarrollado en R, un lenguaje y entorno de programación especializado en análisis estadístico, visualización de datos y modelado. En este repositorio, R se utilizó principalmente para:
+
++ Calcular medidas descriptivas de los datos (media, desviación estándar).
++ Estimar intervalos y porcentajes teóricos de la distribución normal.
++ Comparar los resultados observados contra los teóricos.
++ Medir ajuste a la normalidad mediante asimetría y curtosis.
++ Visualizar resultados en tablas y gráficos.
 
 ## Librerias 
+* _Carpeta Datos:_
+        Contiene archivo de base de datos Hollywood.xls y archivo con los puntos a desarrollar Hollywood.pdf.
+
+* _Carpeta Graficas:_
+
+* _Carpeta Resultados:_
+        Contiene Caso2_Hollywood.pdf y Caso2_Hollywood.docx con el informe de resultados de cada uno de los puntos del caso justificados.
+
+* _Carpeta Scripts:_
+         Contiene un archivo .r para cada uno de los puntos propuestos en Hollywood.pdf.
---
+++ Scripts/Punto 6.r
@@ -0,0 +1,93 @@
+# ------------------------------------------------------
+# Punto 6
+# ------------------------------------------------------
+
+# Cargar librerías
+library(readxl)
+library(tidyverse)
+library(gt)
+
+# Definir ruta del archivo
+ruta <- "C:/Users/LENOVO/OneDrive/Documentos/GitHub/Caso-2-ANALITICA/Datos/Hollywood.xls"
+
+# Importar hoja 
+Datos <- read_excel(ruta, sheet = "Exhibit 1")
+
+# Renombrar columnas para evitar espacios
+names(Datos) <- make.names(names(Datos))
+
+# Modelo inicial (preproducción + estreno)
+model1 <- lm(Opening.Gross ~ Budget + Known.Story + Sequel + MPAA_D +
+               Opening.Theatres + Summer + Holiday + Christmas,
+             data = Datos)
+
+summary(model1)
+
+# Eliminar variables no significativas (p > 0.10)
+model_final <- step(model1, direction = "backward")
+summary(model_final)
+
+# Interpretar coeficientes
+coef(model_final)
+
+# Efecto de +100 cines
+effect_100 <- coef(model_final)["Opening.Theatres"] * 100
+effect_100
+
+# Intervalo de confianza del 95%
+confint(model_final, "Opening.Theatres", level = 0.95) * 100
+
+# Estimación puntual
+effect_100 <- coef(model_final)["Opening.Theatres"] * 100
+
+# Intervalo de confianza
+ci_100 <- confint(model_final, "Opening.Theatres", level = 0.95) * 100
+
+# Crear data frame con resultados
+tabla_effect <- data.frame(
+  Estimacion_Puntual = effect_100,
+  IC_95_Lower = ci_100[1],
+  IC_95_Upper = ci_100[2]
+)
+
+# Convertir en tabla bonita con gt
+tabla_effect_gt <- tabla_effect %>%
+  gt() %>%
+  tab_header(
+    title = "Efecto de incrementar 100 salas",
+    subtitle = "Estimación puntual e intervalo de confianza al 95%"
+  ) %>%
+  fmt_number(
+    columns = everything(),
+    decimals = 0,
+    sep_mark = ","
+  )
+
+# Mostrar tabla
+tabla_effect_gt
+
+# Crear tabla de resultados con coeficientes
+resultados <- summary(model_final)$coefficients
+
+#Tabla de resultados 
+tabla <- resultados %>%
+  as.data.frame() %>%
+  tibble::rownames_to_column("Variable") %>%
+  rename(
+    Estimate = Estimate,
+    StdError = `Std. Error`,
+    t_value  = `t value`,
+    p_value  = `Pr(>|t|)`
+  ) %>%
+  gt() %>%
+  tab_header(
+    title = "Resultados de la regresión final",
+    subtitle = "Predicción del Opening Gross"
+  ) %>%
+  fmt_number(
+    columns = c(Estimate, StdError, t_value, p_value),
+    decimals = 3
+  )
+
+# Mostrar tabla
+tabla
